ee9f49af50ec426a3973f54859a98ba60c1027b3
Delmer Valeriano <valerianodelmer98@gmail.com>
2022-02-22 16:01:46 -0600

    docs agregado

---
+++ docs/precache-manifest.b3764426d9e1e005e567598debec7352.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4a12b65f7eb1c4d63e4019970bc6afc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d91893c34d0ba4f5ded",
+    "url": "/static/js/2.26f5b575.chunk.js"
+  },
+  {
+    "revision": "a90bb751b102ed9d8f9443a23ededd32",
+    "url": "/static/js/2.26f5b575.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "855964d3e5a9ea3a5aca",
+    "url": "/static/js/main.dcf096ed.chunk.js"
+  },
+  {
+    "revision": "46f04600f5238c3977c7",
+    "url": "/static/js/runtime-main.836c258a.js"
+  }
+]);
